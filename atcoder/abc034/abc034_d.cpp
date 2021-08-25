47f9e5fe263159de6cada057c9b5d3e3df8daa95
ruthen71 <takahero2016@gmail.com>
2021-08-25 20:14:38 +0900

    first commit

---
+++ atcoder/abc034/abc034_d.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+//#include <atcoder/all>
+//using namespace atcoder;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<ld, ld>;
+using vii = vector<int>;
+using vll = vector<ll>;
+using vdd = vector<ld>;
+using vvii = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+using vvdd = vector<vector<ld>>;
+using vvvii = vector<vector<vector<int>>>;
+using vvvll = vector<vector<vector<ll>>>;
+using vvvdd = vector<vector<vector<ld>>>;
+template<class T, class U> using P = pair<T, U>;
+template<class T> using V1 = vector<T>;
+template<class T> using V2 = vector<vector<T>>;
+template<class T> using V3 = vector<vector<vector<T>>>;
+template<class T> using pque = priority_queue<T, vector<T>, greater<T>>;
+#define _overload3(_1, _2, _3, name, ...) name
+#define rep1(n) for (ll i = 0; i < (n); i++)
+#define rep2(i, n) for (ll i = 0; i < (n); i++)
+#define rep3(i, a, b) for (ll i = (a); i < (b); i++)
+#define rep(...) _overload3(__VA_ARGS__, rep3, rep2, rep1)(__VA_ARGS__)
+#define rrep1(n) for (ll i = (n) - 1; i >= 0; i--)
+#define rrep2(i, n) for (ll i = (n) - 1; i >= 0; i--)
+#define rrep3(i, a, b) for (ll i = (b) - 1; i >= (a); i--)
+#define rrep(...) _overload3(__VA_ARGS__, rrep3, rrep2, rrep1)(__VA_ARGS__)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define fi first
+#define se second
+#define pb push_back
+#define endl '\n'
+#define popcnt(x) __builtin_popcountll(x)
+#define uniq(x) (x).erase(unique((x).begin(), (x).end()), (x).end());
+#define IOS ios::sync_with_stdio(false); cin.tie(nullptr);
+const int inf = 1 << 30;
+const ll INF = 1ll << 60;
+const ld DINF = numeric_limits<ld>::infinity();
+const ll mod = 1000000007;
+const ll modd = 998244353;
+const ld EPS = 1e-9;
+const ld PI = 3.1415926535897932;
+const ll dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const ll dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+template<class T> bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T> bool chmin(T &a, const T &b) { if (a>b) { a=b; return 1; } return 0; }
+template<typename T> T min(const vector<T> &x) { return *min_element(all(x)); }
+template<typename T> T max(const vector<T> &x) { return *max_element(all(x)); }
+template<class T, class U> ostream &operator<<(ostream &os, const pair<T, U> &p) { return os << "(" << p.fi << ", " << p.se << ")"; }
+template<class T> ostream &operator<<(ostream &os, const vector<T> &v) { os << "[ "; for (auto &z : v) os << z << " "; os << "]"; return os; }
+#define show(x) cout << #x << " = " << x << endl;
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+ll rem(ll a, ll b) { return (a % b + b) % b; }
+// clang-format on
+
+// using mint = modint998244353;
+
+int main() {
+    IOS;
+    ll n, k;
+    cin >> n >> k;
+    V1<ld> w(n), p(n);
+    rep(n) cin >> w[i] >> p[i];
+    ld lb = 0, ub = 100;
+    rep(t, 100) {
+        ld md = (lb + ub) / 2;
+        V1<ld> c(n);
+        rep(i, n) c[i] = (p[i] - md) * w[i];
+        sort(all(c));
+        reverse(all(c));
+        ld s = 0;
+        rep(i, k) s += c[i];
+        if (s >= 0)
+            lb = md;
+        else
+            ub = md;
+    }
+    printf("%.15Lf\n", lb);
+    return 0;
+}
+
+/*
+メモ
+(\sum p_i*w_i) / (\sum w_i) >= P
+より
+\sum (p_i - P) * w_i >= 0
+Pを決め打って条件判定
+*/
